4a61a2f23acc599f2aceade68cf77b5c43273e95
Bruno Henrique <bh.bsb2011@hotmail.com>
2022-02-18 20:14:01 -0300

    sending lambdas

---
+++ backend/lambdas/updatePacient.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk')
+const dynamo = new AWS.DynamoDB.DocumentClient()
+
+exports.handler = async (event) => {
+    const data = JSON.parse(event['body'])
+    try {
+        await dynamo.put({
+            TableName: 'pacients',
+            Item: {
+                name: data.name,
+                email: data.email,
+                address: data.address,
+                date_birth: data.date_birth,
+                id: data.email
+            }
+        }).promise()
+        
+        return {
+            message: 'Updated successfully ' + data.email
+        }
+    } catch (e) {
+        return {
+            error: e
+        }
+    }
+    
+};
